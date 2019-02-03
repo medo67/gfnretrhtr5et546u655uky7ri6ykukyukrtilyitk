c2fb7ceac374b4924d0a86b25117eba9c6d13055
medo67 <45091642+medo67@users.noreply.github.com>
2019-02-04 00:16:17 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ const fs = require("fs");
 const getYoutubeID = require('get-youtube-id');
 const fetchVideoInfo = require('youtube-info');
 const yt_api_key = "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4";
-const discord_token = "حط توكن البوت";
+const discord_token = "NTQxNzM0ODcwODQ2Nzk5ODgy.Dzjxsg.wlgXzmSCerzlvrZpSXc4liPMLeQ";
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
